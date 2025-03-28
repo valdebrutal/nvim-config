612346087197615bbe787d2a2b1dd57c97ad5c9a
intael <carlosvalde9@gmail.com>
2025-03-28 14:43:12 +0200

    add autosave autocmd

--- init.lua
+++ init.lua
@@ -234,6 +234,8 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   command = 'VimtexCompile',
 })
 
+vim.api.nvim_create_autocmd({ 'FocusLost', 'ModeChanged', 'TextChanged', 'BufEnter' }, { desc = 'autosave', pattern = '*', command = 'silent! update' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
